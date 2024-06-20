2c424c79b274361800369209eb22de5c829e9318
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-06-20 16:40:30 -0700

    Pushed simple use of props

---
+++ Journey-To-React-Land/Day3-Props/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BasicHeaderFooter from './components/Basicheaderfooter';
+function App() {
+  
+  return (
+    <>
+   
+    
+    <BasicHeaderFooter name="dark"/>
+    <BasicHeaderFooter name="light mt-5"/>
+    </>
+  );
+}
+
+export default App;
+
