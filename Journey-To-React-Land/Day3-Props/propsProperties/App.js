a646f8406f2aa8f781d9ff5395a1999761e39811
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-06-21 17:02:14 -0700

    Add files via upload

---
+++ Journey-To-React-Land/Day3-Props/propsProperties/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BasicHeaderFooter from './components/Basicheaderfooter';
+function App() {
+  
+  return (
+    <>
+   
+    
+    <BasicHeaderFooter />
+    <BasicHeaderFooter name="light mt-5" title="Footer"/>
+    </>
+  );
+}
+
+export default App;
+
