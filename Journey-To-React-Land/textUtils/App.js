82ea2ed2f503f861b086f2a521af2b202659bfb7
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-06-25 19:19:44 -0700

    tested About Page code

--- Journey-To-React-Land/textUtils/App.js
+++ Journey-To-React-Land/textUtils/App.js
@@ -1,11 +1,14 @@
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextFrom from "./components/TextFrom";
+//import TextFrom from "./components/TextFrom";
 function App() {
   return (
     <>
     <Navbar title="TextUtils"/>
     
-    <div className="container my-3"><TextFrom heading="Enter text here to analyze"/></div>
+    {//<div className="container my-3"><TextFrom heading="Enter text here to analyze"/></div>
+}   
+    <About/>
     </>
   );
 }
