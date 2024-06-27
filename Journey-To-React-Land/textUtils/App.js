8ed96a1313b43725a8dd84312587680fe8f4dcf8
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-06-26 22:59:21 -0700

    Worked on Text Area

--- Journey-To-React-Land/textUtils/App.js
+++ Journey-To-React-Land/textUtils/App.js
@@ -1,14 +1,15 @@
-import About from "./components/About";
+//mport About from "./components/About";
 import Navbar from "./components/Navbar";
-//import TextFrom from "./components/TextFrom";
+import TextFrom from "./components/TextFrom";
 function App() {
   return (
     <>
     <Navbar title="TextUtils"/>
     
-    {//<div className="container my-3"><TextFrom heading="Enter text here to analyze"/></div>
-}   
-    <About/>
+    <div className="container my-3"><TextFrom heading="Enter text here to analyze"/></div>
+   
+   {// <About/> 
+   }
     </>
   );
 }
