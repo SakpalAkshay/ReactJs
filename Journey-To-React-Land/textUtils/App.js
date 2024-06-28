9beb8a4953b3877911fddffb21cbd7e195c2b191
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-06-27 22:06:56 -0700

    Pushed Dark Mode Changes

--- Journey-To-React-Land/textUtils/App.js
+++ Journey-To-React-Land/textUtils/App.js
@@ -1,12 +1,29 @@
 //mport About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextFrom from "./components/TextFrom";
+import React, {useState} from 'react';
+
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode =()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = "#26355D";
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = "white";
+    }
+  }
+
   return (
     <>
-    <Navbar title="TextUtils"/>
+    <Navbar title="TextUtils" mode = {mode} toggleMode = {toggleMode}/> 
+    {//we passed a function as a property to be used in Navbar Component
+    }
     
-    <div className="container my-3"><TextFrom heading="Enter text here to analyze"/></div>
+    <div className="container my-3"><TextFrom heading="Enter text here to analyze" mode = {mode}/></div>
    
    {// <About/> 
    }
