0a3668973f6aa37d479f2f5be32ede76806108df
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-06-29 18:20:23 -0700

    pushed auto dismissing alert

--- Journey-To-React-Land/textUtils/components/Alert.js
+++ Journey-To-React-Land/textUtils/components/Alert.js
@@ -1,1 +1,10 @@
+import React from 'react';
+
+export default function Alerts(props) {
+  return ( //shot circuting as we would provide value once any operation gets performed
+    props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+  <strong>{props.alert.type} : </strong> {props.alert.msg}
+</div>
+  )
+}
 
